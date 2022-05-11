ae5749b782902d7b30d9d3053d7120cec1433b56
Darin Wilson <darinwilson@gmail.com>
2022-05-11 14:15:54 -0700

    :arrow_up: Upgrade to Rails 7

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 require "ucb_rails_user"
@@ -8,7 +9,7 @@ require "ucb_rails_user"
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-  config.load_defaults 5.1
+    config.load_defaults 6.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- ucb_rails_user.gemspec
+++ ucb_rails_user.gemspec
@@ -16,7 +16,8 @@ Gem::Specification.new do |s|
 
   s.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
 
-  s.add_dependency "rails", "> 5.0", "<= 6.1"
+  s.add_dependency "rails", "> 5.0", "< 8.0"
+  s.add_dependency "sprockets-rails"
 
   s.add_dependency "haml", "~> 5.0"
   s.add_dependency "haml-rails", "~> 2.0"
