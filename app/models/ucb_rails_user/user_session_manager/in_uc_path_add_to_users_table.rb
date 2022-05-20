ceb92b415e98c7dc21df3cdd94b7c0b8bbd2e50d
Darin Wilson <darinwilson@gmail.com>
2022-05-20 12:28:16 -0700

    :pencil: Add some code docs

--- app/models/ucb_rails_user/user_session_manager/in_uc_path_add_to_users_table.rb
+++ app/models/ucb_rails_user/user_session_manager/in_uc_path_add_to_users_table.rb
@@ -7,10 +7,13 @@ module UcbRailsUser
     class InUcPathAddToUsersTable < ActiveInUserTable
       def login(uid)
         self.uid = uid
+
+        # try UCPath first
         user = safely_load_user_from_api do
           UcbRailsUser::UserUcPathService.create_or_update_user_from_ldap_uid(self.uid)
         end
 
+        # if that doesn't work, try LDAP
         user ||= safely_load_user_from_api do
           UcbRailsUser::UserLdapService.create_or_update_user_from_entry(people_ou_entry)
         end
