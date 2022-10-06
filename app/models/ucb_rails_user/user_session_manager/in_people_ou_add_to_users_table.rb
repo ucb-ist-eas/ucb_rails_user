a5aefae1cab586131f6fa6b16951f889ba3eee86
Darin Wilson <darinwilson@gmail.com>
2022-10-06 15:08:18 -0700

    :sparkle: Check for valid email when syncing user data from LDAP

--- app/models/ucb_rails_user/user_session_manager/in_people_ou_add_to_users_table.rb
+++ app/models/ucb_rails_user/user_session_manager/in_people_ou_add_to_users_table.rb
@@ -8,11 +8,20 @@ module UcbRailsUser
 
         if people_ou_entry.present?
           UcbRailsUser::UserLdapService.create_or_update_user_from_entry(people_ou_entry).tap do |user|
+            if missing_or_invalid_email?(user)
+              user.update(email: people_ou_entry.alternate_email) if people_ou_entry.alternate_email.present?
+            end
             user.touch(:last_login_at)
           end
         else
           nil
         end
+      end
+
+      private
+
+      def missing_or_invalid_email?(user)
+        user&.email.blank? || (user.email =~ URI::MailTo::EMAIL_REGEXP).nil?
       end
 
     end
