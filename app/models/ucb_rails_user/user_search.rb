be9435011bd058a06a2d439c0cbcacf5d5b07589
Darin Wilson <darinwilson@gmail.com>
2023-03-27 17:10:31 -0700

    :arrow_up: Make changes to work with Rails 7 (#115)
    These mostly have to do with making it play nice with Zeitwerk

--- app/models/ucb_rails_user/user_search.rb
+++ app/models/ucb_rails_user/user_search.rb
@@ -10,10 +10,10 @@ module UcbRailsUser::UserSearch
       .map { |n| "#{n}%" }
     query =
       if name1.present? && name2.present?
-        User.where("LOWER(first_name) LIKE ? AND LOWER(last_name) LIKE ?", name1, name2)
-          .or(User.where("LOWER(last_name) LIKE ? AND LOWER(first_name) LIKE ?", name1, name2))
+        UcbRailsUser::User.where("LOWER(first_name) LIKE ? AND LOWER(last_name) LIKE ?", name1, name2)
+          .or(UcbRailsUser::User.where("LOWER(last_name) LIKE ? AND LOWER(first_name) LIKE ?", name1, name2))
       else
-        User.where("LOWER(first_name) LIKE ? OR LOWER(last_name) LIKE ?", name1, name1)
+        UcbRailsUser::User.where("LOWER(first_name) LIKE ? OR LOWER(last_name) LIKE ?", name1, name1)
       end
     query.order(:last_name, :first_name)
   end
