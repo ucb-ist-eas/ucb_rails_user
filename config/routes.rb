af0a1c80174434f414ab8942497b0426cdab2929
Brandon Zylstra <brandon.zylstra@gmail.com>
2023-11-16 00:32:22 -0500

    Update user routes to work with controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,16 +10,16 @@ Rails.application.routes.draw do
   match "/not_authorized", to: UcbRailsUser::SessionsController.action(:not_authorized),
     as: "not_authorized", via: [:get]
 
-  match "/admin/users/toggle_superuser", to: UcbRailsUser::UsersController.action(:toggle_superuser),
+  match "/admin/users/toggle_superuser", to: UsersController.action(:toggle_superuser),
     as: "admin_toggle_superuser", via: [:get]
 
-  match "/admin/users/search", to: UcbRailsUser::UsersController.action(:search),
+  match "/admin/users/search", to: UsersController.action(:search),
     as: "admin_user_search", via: [:get]
-  match "/admin/users/impersonate_search", to: UcbRailsUser::UsersController.action(:impersonate_search),
+  match "/admin/users/impersonate_search", to: UsersController.action(:impersonate_search),
     as: "admin_user_impersonate_search", via: [:get]
-  match "/admin/users/ldap_search", to: UcbRailsUser::UsersController.action(:ldap_search),
+  match "/admin/users/ldap_search", to: UsersController.action(:ldap_search),
     as: "admin_user_ldap_search", via: [:get]
-  resources :users, controller: "ucb_rails_user/users", path: "admin/users", as: :admin_users
+  resources :users, controller: "users", path: "admin/users", as: :admin_users
 
   resources :impersonations, controller: "ucb_rails_user/impersonations", path: "admin/impersonations", as: :admin_impersonations
   match "/admin/stop_impersonation", to: UcbRailsUser::ImpersonationsController.action(:stop_impersonation),
